92b11f7c85efbf156300406e68f2bfb7f0938b50
Hyan Lucas <hyanlucas2011@gmail.com>
2024-06-15 13:36:34 -0300

    Discover and Visualize the Data to Gain Insights

--- utils/download_extract_data.py
+++ utils/download_extract_data.py
@@ -2,10 +2,10 @@ import os
 import tarfile
 import urllib
 
-def fetch_housing_data(housing_url, housing_path):
-    os.makedirs(housing_path, exist_ok=True)
-    tgz_path = os.path.join(housing_path, "housing.tgz")
-    urllib.request.urlretrieve(housing_url, tgz_path)
+def fetch_housing_data(url, path, name):
+    os.makedirs(path, exist_ok=True)
+    tgz_path = os.path.join(path, name)
+    urllib.request.urlretrieve(url, tgz_path)
     housing_tgz = tarfile.open(tgz_path)
-    housing_tgz.extractall(path=housing_path)
+    housing_tgz.extractall(path=path)
     housing_tgz.close()
